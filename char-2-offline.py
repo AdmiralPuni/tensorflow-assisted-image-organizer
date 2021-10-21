8d908bb4ce01f305c5c4ccae2ee02babaeaea2da
AdmiralPuni <drfredmagic@gmail.com>
2021-10-21 10:34:22 +0700

    performance optimization, new model
    image is now loaded and predicted before the judgement

--- char-2-offline.py
+++ char-2-offline.py
@@ -5,10 +5,11 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 import tensorflow.keras as keras
 import json
-from shutil import copyfile
+from shutil import move
 import numpy as np
 from keras.preprocessing import image
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 
 selected_model = ""
@@ -17,6 +18,21 @@ model_filename = ""
 settings_filename = "settings.json"
 settings_json = json.load(open(settings_filename, 'r'))
 input_directory = 'input'
+output_directory = 'output'
+model_directory = 'models'
+
+prediction_list = []
+
+class prediction:
+  def __init__(self, filename, classes, image):
+    self.filename = filename
+    self.classes = classes
+    self.image = image
+
+plt.ion()
+plt.show()
+plt.imshow(image.load_img('logpu.png', target_size=(150, 150)))
+
 
 print('TAIO v0.1 TERMINAL')
 print('==============================================')
@@ -24,17 +40,23 @@ print('==============================================')
 for index, model_name in enumerate(settings_json['models']):
   print(model_name)
 
+print('==============================================')
+
 selected_model = input('Select model : ')
+
 character_names = settings_json['models'][selected_model]['names']
 model_filename = settings_json['models'][selected_model]['filename']
 
-#create output folder based on names
-if not os.path.exists('output/' + selected_model + '/false'):
-    os.makedirs('output/' + selected_model + '/false')
+print('==============================================')
+print('Predicting images...')
 
+
+
+
+#create output folder based on names
 for name in character_names:
-  if not os.path.exists('output/' + selected_model + '/' + name):
-    os.makedirs('output/' + selected_model + '/' + name)
+  if not os.path.exists(output_directory + '/' + selected_model + '/' + name):
+    os.makedirs(output_directory + '/' + selected_model + '/' + name)
 
 #load keras model
 model = keras.models.Sequential([
@@ -52,41 +74,40 @@ model = keras.models.Sequential([
 ])
 
 #load saved model
-model.load_weights("models/" + model_filename)
+model.load_weights(model_directory + "/" + model_filename)
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-def user_decision(detected_class, filename, accuracy):
+def user_decision(detected_class, filename):
   print("Detected  :", character_names[detected_class])
-  print("Accuracy  :", round(accuracy)*100)
+  plt.xlabel(character_names[detected_class].upper(), fontsize=30)
   if input("3=true    : ") == "3":
-    copyfile(path, "output/" + selected_model + '/' + character_names[detected_class] + "/" + filename)
-  else:
-    copyfile(path, "output/" + selected_model + '/false/' + filename)
-
+    move(os.path.join(input_directory, filename), output_directory + "/" + selected_model + '/' + character_names[detected_class] + "/" + filename)
 #load saved model
-plt.ion()
-plt.show()
-for filename in os.listdir(input_directory):
+
+for filename in tqdm(os.listdir(input_directory)):
   if filename.endswith(".jpg") or filename.endswith(".png"):
     path = os.path.join(input_directory, filename)
     img = image.load_img(path, target_size=(150, 150))
-    imgplot = plt.imshow(img)
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
 
     images = np.vstack([x])
     classes = model.predict(images, batch_size=10)
 
-    print('==============================================')
-    print("File      :", filename)
-
-    largest = 0
-
-    for x in range(0, len(classes[0])):
-      if(classes[0][x] > largest):
-        largest = x
-
-    user_decision(largest, filename, classes[0][largest])
-
+    prediction_list.append(prediction(filename, classes, img))
   else:
       continue
+
+for list in prediction_list:
+  plt.imshow(list.image)
+
+  print('==============================================')
+  print("File      :", list.filename)
+
+  largest = 0
+
+  for x in range(0, len(list.classes[0])):
+    if(list.classes[0][x] > largest):
+      largest = x
+
+  user_decision(largest, list.filename)
